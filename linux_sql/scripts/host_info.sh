5dd375a8fa0a7f23b9f7071e1748a1252429eace
Nissa Elchhab <39413995+RtillaWork@users.noreply.github.com>
2022-06-15 14:50:54 +0000

    change: extract a generic function; add: assign local PGPASSWORD for psql invoke

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -3,7 +3,7 @@
 # collects hardware information about host on deployment;
 # then updates database (psql)
 # data collected:
-# id,hostname,cpu_number,cpu_architecture,cpu_model,cpu_mhz,L2_cache,timestamp
+# id, hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, timestamp
 # TODO pre-requisites: Linux "tools" default on Centos 7 or compatible;  add details about libs
 # TODO embed key/certificate to communicate securely with db
 # TODO verify db connectivity, otherwise, cache info locally, temporarily and encrypted and...
@@ -11,6 +11,7 @@
 # TODO include local hardware refresh timer, or possibly, detect hardware changes (hash change?)
 # TODO include expiration time
 
+# global constants
 debug=1
 # ./scripts/host_info.sh psql_host psql_port db_name psql_user psql_password
 psql_host=$1
@@ -18,6 +19,7 @@ psql_port=$2
 db_name=$3
 psql_user=$4
 psql_password=$5
+psql_table_name='hosts_info'
 
 if [ $# -ne 5 ]; then
   echo "Error: wrong number of arguments"
@@ -25,33 +27,41 @@ if [ $# -ne 5 ]; then
   exit 1
 fi
 
+
 # lscpu fields
+lscpu_info=$(lscpu) # lscpu is part of util-linux package; ftp://ftp.kernel.org/pub/linux/utils/util-linux/
 cpu_number_key="^CPU\(s\):"
 cpu_architecture_key="^Architecture:"
 cpu_model_key="^Model\ name:"
 cpu_mhz_key="^CPU MHz:"
 L2_cache_key="^L2\ cache:"
-# free
-total_mem_key="^Mem:"
+lscpu_value () {
+  echo "$lscpu_info" | egrep "$1" | awk --field-separator ":" '{print $2}' | xargs
+}
 
 hostname=$(hostname -f) #fully qualified hostname
-cpu_number=$(echo "$(lscpu)" | egrep "$cpu_number_key" | awk --field-separator ":" '{print $2}' | xargs)
-cpu_architecture=$(echo "$(lscpu)" | egrep "$cpu_architecture_key" | awk --field-separator ":" '{print $2}' | xargs)
-cpu_model=$(echo "$(lscpu)" | egrep "$cpu_model_key" | awk --field-separator ":" '{print $2}' | xargs)
-cpu_mhz=$(echo "$(lscpu)" | egrep "$cpu_mhz_key" | awk  --field-separator ":" '{print $2}' | xargs)
-L2_cache=$(echo "$(lscpu)" | egrep "$L2_cache_key" | awk  --field-separator ":" '{print $2}' | sed 's/K//' | xargs)     #in kB
-total_mem=$(echo "$(free --kilo)" | egrep "$total_mem_key" | awk  --field-separator " " '{print $2}' | xargs)
+cpu_number=$(lscpu_value "$cpu_number_key")
+cpu_architecture=$(lscpu_value "$cpu_architecture_key")
+cpu_model=$(lscpu_value "$cpu_model_key")
+cpu_mhz=$(lscpu_value "$cpu_mhz_key")
+L2_cache=$( echo $(lscpu_value "$L2_cache_key") | sed 's/K//')  # in kB
+total_mem=$(echo "$(free --kilo)" | egrep "^Mem:" | awk  --field-separator " " '{print $2}' | xargs)
 timestamp=$(TZ="UTC0" date '+%F %T')  #Current time in UTC time zone TODO read from vmstat instead
 
+# build SQL command
+sql_cmd="INSERT INTO $psql_table_name" # TODO verify if table name should be quoted instead
+sql_cmd="$sql_cmd (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, timestamp)"
+sql_cmd="$sql_cmd VALUES ($hostname, $cpu_number, $cpu_architecture, $cpu_model, $cpu_mhz, $L2_cache, $timestamp);"
+psql_invoke="PGPASSWORD=$psql_password psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -w  -c \"$sql_cmd\" "
+# NOTE alternative  postgresql://[user[:password]@][host][:port][/dbname][?param1=value1&...]
+
 if [ $debug -eq 1 ]; then
-  echo $hostname
-  echo $cpu_number
-  echo $cpu_architecture
-  echo $cpu_model
-  echo $cpu_mhz
-  echo $L2_cache
-  echo $total_mem
-  echo $timestamp
+  echo "$sql_cmd"
+  echo
+  echo "$psql_invoke"
+  echo
 fi
 
-exit 0
+eval "$psql_invoke"
+# TODO add caching, alert or retries on exit code != 0
+exit $?
