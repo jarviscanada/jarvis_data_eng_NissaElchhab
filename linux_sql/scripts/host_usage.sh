b1b4492ba2e267d6557c3fd276338b8c51d72909
Nissa Elchhab <39413995+RtillaWork@users.noreply.github.com>
2022-06-15 11:26:01 +0000

    change: use vmstat for reading resources for more accuracy; add debug flag for echo'ing values

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -11,20 +11,22 @@
 # TODO include expiration time
 # TODO add missing/NIL values in case not all resources could have been collected at once
 
+debug=1
 
-timestamp=$(TZ="UTC0" date '+%F %T') #UTC time zone
-host_id=1                     #host id from `hosts` table
-memory_free=$(cat /proc/meminfo | egrep "^MemFree:" | awk --field-separator " " '{print $2}' | xargs)
-memory_free=$((memory_free / 1024))
+timestamp=$(vmstat -t | tail -1 | awk --field-separator " " '{print  $18 " " $19}' | xargs) #UTC time zone
+host_id=$(hostid)                     #host id from `hosts` table
+memory_free=$(vmstat -t --unit M | tail -1 | awk --field-separator " " '{print $4}' | xargs)
 cpu_idle=$(vmstat | tail -1 | awk --field-separator " " '{print $15}' | xargs)                   #in percentage
 cpu_kernel=$(vmstat | tail -1 | awk --field-separator " " '{print $14}' | xargs)                  #in percentage
 disk_io=$(vmstat -d | egrep "^sd[a-z]" | awk --field-separator " " '{print $10}' | xargs)   #number of disk I/O
 disk_available=$(df -BM / | egrep "^\/" | awk --field-separator " " '{print $4}' | sed 's/M//' | xargs) #in MB. root directory avaiable disk
 
-echo $timestamp
-echo $host_id
-echo $memory_free
-echo $cpu_idle
-echo $cpu_kernel
-echo $disk_io
-echo $disk_available
+if [ $debug -eq 1 ]; then
+  echo $timestamp
+  echo $host_id
+  echo $memory_free
+  echo $cpu_idle
+  echo $cpu_kernel
+  echo $disk_io
+  echo $disk_available
+fi
