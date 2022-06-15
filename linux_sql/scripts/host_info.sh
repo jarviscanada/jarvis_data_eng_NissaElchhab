5cc5928b62ca0f9b384d3f5a35183ab2fe6ef910
Nissa Elchhab <39413995+RtillaWork@users.noreply.github.com>
2022-06-15 11:32:22 +0000

    change: add debug flag and debug conditional values echo'ing

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -6,10 +6,12 @@
 # id,hostname,cpu_number,cpu_architecture,cpu_model,cpu_mhz,L2_cache,timestamp
 # TODO pre-requisites: Linux "tools" default on Centos 7 or compatible;  add details about libs
 # TODO embed key/certificate to communicate securely with db
-# TODO verify db connectivity, otherwise, cachge info lcoally, temporarily and encrypted and...
+# TODO verify db connectivity, otherwise, cache info locally, temporarily and encrypted and...
 # TODO upload at first opportunity;
 # TODO include local hardware refresh timer, or possibly, detect hardware changes (hash change?)
 # TODO include expiration time
+
+debug=1
 
 # lscpu fields
 cpu_number_key="^CPU\(s\):"
@@ -20,7 +22,6 @@ L2_cache_key="^L2\ cache:"
 # free
 total_mem_key="^Mem:"
 
-# id=1      #psql db auto-increment
 hostname=$(hostname -f) #fully qualified hostname
 cpu_number=$(echo "$(lscpu)" | egrep "$cpu_number_key" | awk --field-separator ":" '{print $2}' | xargs)
 cpu_architecture=$(echo "$(lscpu)" | egrep "$cpu_architecture_key" | awk --field-separator ":" '{print $2}' | xargs)
@@ -30,11 +31,13 @@ L2_cache=$(echo "$(lscpu)" | egrep "$L2_cache_key" | awk  --field-separator ":" 
 total_mem=$(echo "$(free --kilo)" | egrep "$total_mem_key" | awk  --field-separator " " '{print $2}' | xargs)
 timestamp=$(TZ="UTC0" date '+%F %T')  #Current time in UTC time zone TODO read from vmstat instead
 
-echo $hostname
-echo $cpu_number
-echo $cpu_architecture
-echo $cpu_model
-echo $cpu_mhz
-echo $L2_cache
-echo $total_mem
-echo $timestamp
+if [ $debug -eq 1 ]; then
+  echo $hostname
+  echo $cpu_number
+  echo $cpu_architecture
+  echo $cpu_model
+  echo $cpu_mhz
+  echo $L2_cache
+  echo $total_mem
+  echo $timestamp
+fi
