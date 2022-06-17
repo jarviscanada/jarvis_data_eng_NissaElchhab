4c7e2bbd64e1f378fd44fab195796767b2a8c243
Nissa Elchhab <39413995+RtillaWork@users.noreply.github.com>
2022-06-17 08:07:06 +0000

    add initial DDL commands in ddl.sql; tested working using psql shell and psql command line with -f option

---
+++ linux_sql/sql/ddl.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
+(
+    host_id        SERIAL    NOT NULL,
+    memory_free    INTEGER   NOT NULL,
+    cpu_idle       INTEGER   NOT NULL,
+    cpu_kernel     INTEGER   NOT NULL,
+    disk_io        INTEGER   NOT NULL,
+    disk_available INTEGER   NOT NULL,
+    "timestamp"    TIMESTAMP NOT NULL,
+    PRIMARY KEY (host_id),
+    CONSTRAINT valid_discrete_values CHECK ( memory_free >= 0 AND disk_io >= 0 AND disk_available >= 0),
+    CONSTRAINT valid_percentage CHECK ( (cpu_idle BETWEEN 0 AND 100) AND (cpu_kernel BETWEEN 0 AND 100))
+);
+
+CREATE TABLE IF NOT EXISTS PUBLIC.host_info
+(
+    id               SERIAL       NOT NULL,
+    hostname         VARCHAR(256) NOT NULL,
+    cpu_number       INTEGER      NOT NULL,
+    cpu_architecture VARCHAR      NOT NULL,
+    cpu_model        VARCHAR      NOT NULL,
+    cpu_mhz          REAL         NOT NULL,
+    L2_cache         INTEGER      NOT NULL,
+    total_mem        INTEGER      NOT NULL,
+    "timestamp"      TIMESTAMP    NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (hostname),
+    CONSTRAINT valid_discrete_value CHECK ( cpu_number > 0 AND L2_cache >= 0 AND total_mem >= 0 ),
+    CONSTRAINT valid_frequency CHECK ( cpu_mhz >= 0.0 AND cpu_mhz <= 10000.0 )
+    );
+
