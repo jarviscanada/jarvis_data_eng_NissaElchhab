b7cb00bec3123cddcd6c5555fa8ca386ecf4ec16
Nissa Elchhab <39413995+RtillaWork@users.noreply.github.com>
2022-06-22 07:05:32 +0000

    fix second query by adding host_id to PARTITION BY

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -6,27 +6,35 @@ ORDER BY cpu_number ASC, total_mem DESC;
 
 -- Average used memory in percentage over 5 mins interval for each host. (used memory = total memory - free memory).
 -- helper functions
-CREATE FUNCTION round_ts_5min(ts timestamp) RETURNS timestamp AS
+CREATE FUNCTION round_ts_5min(ts TIMESTAMP) RETURNS timestamp AS
     $$
 BEGIN
-RETURN date_trunc('hour', ts) + date_part('minute', ts)::int / 5 * INTERVAL '5 min';
+RETURN date_trunc('hour', ts) + date_part('minute', ts)::INT / 5 * INTERVAL '5 min';
 END;
     $$
 LANGUAGE PLPGSQL;
 END;
-CREATE FUNCTION used_memory_pcnt(total_mem bigint, free_mem integer) RETURNS real AS
-    $$BEGIN RETURN ((total_mem::double precision - free_mem) / total_mem) * 100; END;$$
-LANGUAGE PLPGSQL;
-end;
 
-SELECT host_id, hostname, timestamp_5min_intervals, avg_used_memory_percent
+CREATE FUNCTION used_memory_pcnt(total_mem BIGINT, free_mem INTEGER) RETURNS REAL AS
+    $$BEGIN RETURN ((total_mem::DOUBLE PRECISION - free_mem) / total_mem) * 100; END;$$
+LANGUAGE PLPGSQL;
+END;
+
+SELECT time_interval_table.host_id host_id,
+       time_interval_table.hostname hostname,
+       time_interval_table.timestamp_5min_intervals "timestamp",
+       time_interval_table.avg_used_memory_percent avg_used_memory_percent
 FROM (
-         SELECT u.host_id, i.hostname,round_ts_5min(u.timestamp) AS timestamp_5min_intervals,
+         SELECT u.host_id,
+                i.hostname,
+                round_ts_5min(u.timestamp) AS timestamp_5min_intervals,
                 avg( used_memory_pcnt(i.total_mem, u.memory_free*1024))
-                OVER( PARTITION BY round_ts_5min(u.timestamp) ) as avg_used_memory_percent
-         FROM host_usage u INNER JOIN host_info i ON u.host_id = i.id) AS time_interval_table
-GROUP BY host_id, hostname, timestamp_5min_intervals, avg_used_memory_percent
-ORDER BY host_id , timestamp_5min_intervals;
+                                              OVER( PARTITION BY u.host_id, round_ts_5min(u.timestamp) )
+                    AS avg_used_memory_percent
+         FROM host_usage u INNER JOIN host_info i ON u.host_id = i.id
+     ) AS time_interval_table
+GROUP BY host_id, hostname, "timestamp", avg_used_memory_percent
+ORDER BY host_id , "timestamp";
 
 -- The cron job is supposed to insert a new entry to the host_usage table every minute when the server is healthy.
 -- We can assume that a server is failed when it inserts less than three data points within 5-min interval.
@@ -35,7 +43,8 @@ ORDER BY host_id , timestamp_5min_intervals;
 SELECT data_points.host_id host_id, data_points.ts ts, data_points.num_data_points num_data_points
 FROM (
          SELECT u.host_id host_id, round_ts_5min(u.timestamp) ts,
-                count(*) over(partition by u.host_id, round_ts_5min(u.timestamp)) AS num_data_points
-         FROM host_usage u) AS data_points
+                count(*) OVER (PARTITION BY u.host_id, round_ts_5min(u.timestamp)) AS num_data_points
+         FROM host_usage u
+         ) AS data_points
 GROUP BY host_id, ts, num_data_points
 ORDER BY host_id, ts, num_data_points;
