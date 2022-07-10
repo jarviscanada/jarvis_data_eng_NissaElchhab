37c700f431eb16d6c9c22524a126059b4c2fd2fe
Nissa Elchhab <39413995+RtillaWork@users.noreply.github.com>
2022-07-10 22:03:25 +0000

    define RegexExc interface

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExc.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExc.java
@@ -1,4 +1,26 @@
 package ca.jrvs.apps.practice;
 
 public interface RegexExc {
+
+    /**
+     * return true if file extension is `jpeg` or `jpg` (case-insensitive)
+     * @param filename
+     * @return
+     */
+    public boolean matchJpeg(String filename);
+
+    /**
+     * return true if ip is valid
+     * to simplify the problem, an ip address ranges from 0.0.0.0 to 999.999.999.999
+     * @param ip
+     * @return
+     */
+    public boolean matchIp(String ip);
+
+    /**
+     * return true if line is empty (e.g. empty, white space, tabs, etc...)
+     * @param line
+     * @return
+     */
+    public boolean isEmptyLine(String line);
 }
