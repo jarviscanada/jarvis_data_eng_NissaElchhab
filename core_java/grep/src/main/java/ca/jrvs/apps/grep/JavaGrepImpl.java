a90ec2271a26d6605e5f89afd90d68e7aef7f353
Nissa Elchhab <39413995+RtillaWork@users.noreply.github.com>
2022-07-24 17:30:30 -0600

    fix issues related to logger SLF4j packaging

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImpl.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImpl.java
@@ -1,7 +1,7 @@
 package ca.jrvs.apps.grep;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
