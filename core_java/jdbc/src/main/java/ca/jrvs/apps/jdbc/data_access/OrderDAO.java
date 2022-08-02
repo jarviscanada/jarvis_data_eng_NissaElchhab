306b9d27b5cb136b846afa876c9e61efab9f1476
Nissa Elchhab <39413995+RtillaWork@users.noreply.github.com>
2022-08-02 06:32:35 -0600

    add SQL statement for OrderDAO#findById

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/data_access/OrderDAO.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/data_access/OrderDAO.java
@@ -6,14 +6,29 @@ import java.util.List;
 
 public class OrderDAO extends DAO<Order> {
 
+  private static final String FIND_ONE = "SELECT "
+      + "c.first_name, c.last_name, c.email, o.order_id, "
+      + "o.creation_date, o.total_due, o.status, "
+      + "s.first_name, s.last_name, s.email, "
+      + "ol.quantity, "
+      + "p.code, p.name, p.size, p.variety, p.price "
+      + "from orders o "
+      + "join customer c on o.customer_id = c.customer_id "
+      + "join salesperson s on o.salesperson_id=s.salesperson_id "
+      + "join order_item ol on ol.order_id=o.order_id "
+      + "join product p on ol.product_id = p.product_id "
+      + "where o.order_id = ?";
+
   public OrderDAO(Connection connection) {
     super(connection);
   }
 
   @Override
   public Order findById(Long id) {
+
     return null;
   }
+
 
   @Override
   public List<Order> findAll() {
