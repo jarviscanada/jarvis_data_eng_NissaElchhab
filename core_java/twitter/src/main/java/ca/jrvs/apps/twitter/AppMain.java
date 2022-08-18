d7789da5c73d5a6a3fe9a14343d1ac315b0af7e8
Nissa Elchhab <39413995+RtillaWork@users.noreply.github.com>
2022-08-18 07:28:48 -0600

    add twitter data dictionary/reference object URLs for documentation

---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/AppMain.java
@@ -0,0 +1,8 @@
+package ca.jrvs.apps.twitter;
+
+class AppMain {
+
+public static void main(String[]args){
+
+    }
+}
--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Tweet.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Tweet.java
@@ -1,5 +1,13 @@
 package ca.jrvs.apps.twitter.model;
 
+/**
+ * Reference:
+ * https://developer.twitter.com/en/docs/twitter-api/v1/data-dictionary/overview
+ * https://developer.twitter.com/en/docs/twitter-api/v1/data-dictionary/object-model/tweet
+ * https://developer.twitter.com/en/docs/twitter-api/v1/data-dictionary/object-model/user
+ * https://developer.twitter.com/en/docs/twitter-api/v1/data-dictionary/object-model/geo
+ */
+
 public class Tweet {
 
 }
