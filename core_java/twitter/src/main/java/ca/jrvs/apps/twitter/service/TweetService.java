72ef03ffaf964d08ddead6ef773737eeb6add73c
Nissa Elchhab <39413995+RtillaWork@users.noreply.github.com>
2022-08-30 12:47:57 -0600

    initial TweetService

---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/service/TweetService.java
@@ -0,0 +1,45 @@
+package ca.jrvs.apps.twitter.service;
+
+import ca.jrvs.apps.twitter.model.Tweet;
+import java.util.List;
+
+public class TweetService implements Service{
+
+  /**
+   * Validate and post a user input Tweet
+   *
+   * @param tweet tweet to be created
+   * @return created tweet
+   * @throws IllegalArgumentException if text exceed max number of allowed characters or lat/long
+   *                                  out of range
+   */
+  @Override
+  public Tweet postTweet(Tweet tweet) {
+    return null;
+  }
+
+  /**
+   * Search a tweet by ID
+   *
+   * @param id     tweet id
+   * @param fields set fields not in the list to null
+   * @return Tweet object which is returned by the Twitter API
+   * @throws IllegalArgumentException if id or fields param is invalid
+   */
+  @Override
+  public Tweet showTweet(String id, String[] fields) {
+    return null;
+  }
+
+  /**
+   * Delete Tweet(s) by id(s).
+   *
+   * @param ids tweet IDs which will be deleted
+   * @return A list of Tweets
+   * @throws IllegalArgumentException if one of the IDs is invalid.
+   */
+  @Override
+  public List<Tweet> deleteTweets(String[] ids) {
+    return null;
+  }
+}
---
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/service/TweetServiceTest.java
@@ -0,0 +1,25 @@
+package ca.jrvs.apps.twitter.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TweetServiceTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void postTweet() {
+  }
+
+  @Test
+  public void showTweet() {
+  }
+
+  @Test
+  public void deleteTweets() {
+  }
+}
