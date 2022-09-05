bebd3c7ca1aa61d30448b007a5f222c6dd5032c8
Nissa Elchhab <39413995+RtillaWork@users.noreply.github.com>
2022-09-05 09:11:35 -0600

    initial Controller

---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/controller/TweetController.java
@@ -0,0 +1,43 @@
+package ca.jrvs.apps.twitter.controller;
+
+import ca.jrvs.apps.twitter.model.Tweet;
+import java.util.List;
+
+public class TweetController implements Controller{
+
+  /**
+   * Parse user argument and post a tweet by calling service classes
+   *
+   * @param args
+   * @return a posted tweet
+   * @throws IllegalArgumentException if args are invalid
+   */
+  @Override
+  public Tweet postTweet(String[] args) {
+    return null;
+  }
+
+  /**
+   * Parse user argument and search a tweet by calling service classes
+   *
+   * @param args
+   * @return a tweet
+   * @throws IllegalArgumentException if args are invalid
+   */
+  @Override
+  public Tweet showTweet(String[] args) {
+    return null;
+  }
+
+  /**
+   * Parse user argument and delete tweets by calling service classes
+   *
+   * @param args
+   * @return a list of deleted tweets
+   * @throws IllegalArgumentException if args are invalid
+   */
+  @Override
+  public List<Tweet> deleteTweet(String[] args) {
+    return null;
+  }
+}
