fa95f247c4ce61733caea810f0506fd8a96f7a36
Nissa Elchhab <39413995+RtillaWork@users.noreply.github.com>
2022-09-06 03:50:38 -0600

    add @Autowired default constructor to Tweet validator

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/validation/Tweet.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/validation/Tweet.java
@@ -3,6 +3,7 @@ package ca.jrvs.apps.twitter.validation;
 import ca.jrvs.apps.twitter.model.Coordinates;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +18,10 @@ public class Tweet extends ca.jrvs.apps.twitter.model.Tweet implements
 
   public static final Logger logger = LoggerFactory.getLogger(
       ca.jrvs.apps.twitter.model.Tweet.class);
+
+  @Autowired
+  public Tweet() {
+  }
 
   private final Validator<Coordinates> coordinatesValidator =
       new ca.jrvs.apps.twitter.validation.Coordinates();
