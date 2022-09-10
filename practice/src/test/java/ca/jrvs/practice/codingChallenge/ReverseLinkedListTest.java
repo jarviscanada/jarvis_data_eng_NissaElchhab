c71fe0f500d1b7e7e9f5f61d87f89238b6f76459
Nissa Elchhab <39413995+RtillaWork@users.noreply.github.com>
2022-09-10 14:43:18 -0600

    add tests

---
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/ReverseLinkedListTest.java
@@ -0,0 +1,17 @@
+package ca.jrvs.practice.codingChallenge;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class ReverseLinkedListTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+}
