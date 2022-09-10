6c793da5cfb31b5541cda4b2d8adee6d12a4f564
Nissa Elchhab <39413995+RtillaWork@users.noreply.github.com>
2022-09-10 14:53:50 -0600

    add utility class test

---
+++ practice/src/test/java/ca/jrvs/practice/ListNodeTest.java
@@ -0,0 +1,17 @@
+package ca.jrvs.practice;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class ListNodeTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+}
