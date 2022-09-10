e2903f586fb439568f5f8016a3c6723872ddde7e
Nissa Elchhab <39413995+RtillaWork@users.noreply.github.com>
2022-09-09 20:30:44 -0600

    add solution; procedural loop version

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/ReverseLinkedList.java
@@ -0,0 +1,54 @@
+package ca.jrvs.practice.codingChallenge;
+
+import ca.jrvs.practice.ListNode;
+
+/**
+ * Ticket:
+ * https://www.notion.so/jarvisdev/Nissa-Coding-Challenges-4-questions-per-week-4f714ddda1c740deb052485e3ddf9576?p=8372f77aea2948a7b8a9892c65af1d42&pm=s
+ */
+public class ReverseLinkedList {
+
+  /**
+   * Definition for singly-linked list. public class ListNode { int val; ListNode next; ListNode()
+   * {} ListNode(int val) { this.val = val; } ListNode(int val, ListNode next) { this.val = val;
+   * this.next = next; } }
+   */
+  class Solution {
+
+    /**
+     * procedural loop version
+     * https://leetcode.com/submissions/detail/795937700/
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList1(ListNode head) {
+      // We code empty linkedlist with null head
+      if (head == null) {
+        return null;
+      }
+
+      ListNode reverseHead = new ListNode(head.val, null);
+      for (ListNode h = head.next; ; h = h.next) {
+        if (h != null) {
+          ListNode reverseNode = new ListNode(h.val, reverseHead);
+          reverseHead = reverseNode;
+        } else {
+          break;
+        }
+
+      }
+      return reverseHead;
+    }
+
+    /**
+     * recursive version
+     * @param head
+     * @return
+     */
+    public ListNode reverseList2(ListNode head) {
+      return null;
+  }
+
+}
+}
